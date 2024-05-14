be2064663a6b48bf69c6fc626550210c6a61f1f2
akankshagite072 <150998236+akankshagite072@users.noreply.github.com>
2024-05-14 12:19:14 +0530

    Update App.java

--- hibernate_pgm/src/main/java/com/sms/hibernate_pgm/App.java
+++ hibernate_pgm/src/main/java/com/sms/hibernate_pgm/App.java
@@ -1,5 +1,10 @@
 package com.sms.hibernate_pgm;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+
+        // Your Hibernate code logic here
+
+        t.commit();
+        session.close();
+        factory.close();
     }
 }
